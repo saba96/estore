f7ed95cbccafa1ac3dc7421daf7d2b9f7269da6b
Saba Ahmadi <saba.ahmadi96@gmail.com>
2018-08-29 16:42:02 +0430

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+let path = require('path')
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/store',
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/test')
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/store_test',
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/development')
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/production')
+    }
+  }
+}
