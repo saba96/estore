ff9bad13de299797ef22f88833d18ee4ac35589c
Saba Ahmadi <saba.ahmadi96@gmail.com>
2018-08-30 01:20:43 +0430

    Add script for nodemon Add migration for customers

--- db/migrations/20180829152906_customers.js
+++ db/migrations/20180829152906_customers.js
@@ -1,8 +1,9 @@
 
 exports.up = function (knex, Promise) {
-
+  return knex.schema.createTable('customers', table => {
+  })
 }
 
 exports.down = function (knex, Promise) {
-
+  return knex.schema.dropTable('customers')
 }
